5c0efaeb0118b95c734deddcd09bf8913d003087
Amir Dashti <ahdashti@gmail.com>
2023-01-19 02:30:41 +0330

    fix activate

--- src/app/pages/activate/activate.page.ts
+++ src/app/pages/activate/activate.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-activate',
@@ -7,12 +9,54 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./activate.page.scss'],
 })
 export class ActivatePage implements OnInit {
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute,
+    private loadingController: LoadingController,
+    private router: Router,
+    private alertController: AlertController,
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  async verify_account() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
     const uid = this.route.snapshot.paramMap.get('uid');
     const token = this.route.snapshot.paramMap.get('token');
-    console.log('uid = ', uid);
-    console.log('token = ', token);
+
+    this.apiService.verify(uid,token).subscribe({
+      next: async (v) => {
+        await loading.dismiss();
+        console.log(v)
+        const alert = await this.alertController.create({
+          header: 'successful',
+          message: 'Your account has been created and is ready to use!',
+          buttons: ['OK'],
+        });
+    
+        await alert.present();
+      
+        this.router.navigateByUrl('/', { replaceUrl: true });
+      },
+      error: async (error) => {
+        await loading.dismiss();
+
+        const alert = await this.alertController.create({
+          header: 'error',
+          message: 'error',
+          buttons: ['OK'],
+        });
+    
+        await alert.present();
+        
+
+      
+      },
+    })
+
+    // verify(uid, token);
+    // setVerified(true);
   }
 }
